f884fa6a7704ee4368569bb1cb6f332d62492e51
liblaf <30631553+liblaf@users.noreply.github.com>
2024-03-16 21:58:07 +0800

    feat: integrate local search plugin for improved search functionality
    Added the "@easyops-cn/docusaurus-search-local" plugin to enhance search capabilities within the site, providing features like highlighting search terms and explicit search result paths. This change aims to improve user experience and make content more discoverable.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import { PluginOptions as SearchPluginOptions } from "@easyops-cn/docusaurus-search-local";
 
 const config: Config = {
   title: "Assets",
@@ -8,20 +9,40 @@ const config: Config = {
   favicon: "/letter/ico/a.ico",
 
   themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
     navbar: {
       title: "Assets",
       logo: {
+        alt: "Logo",
         src: "/letter/svg/a.svg",
       },
       items: [
         {
-          href: "https://github.com/liblaf/assets",
           label: "GitHub",
+          href: "https://github.com/liblaf/assets",
+          position: "right",
+        },
+        {
+          type: "search",
           position: "right",
         },
       ],
     },
   } satisfies Preset.ThemeConfig,
+
+  themes: [
+    [
+      "@easyops-cn/docusaurus-search-local",
+      {
+        docsRouteBasePath: "/",
+        hashed: true,
+        highlightSearchTermsOnTargetPage: true,
+        explicitSearchResultPath: true,
+      } satisfies SearchPluginOptions,
+    ],
+  ],
 
   presets: [
     [
